a85086922dcee0add5cb7b0c932c664a7f330ffb
Ulisses Alves <ulisalves@gmail.com>
2023-04-06 11:23:23 -0300

    final

--- desafio-poo-java/src/main/java/Main.java
+++ desafio-poo-java/src/main/java/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -23,5 +25,36 @@ public class Main {
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devUlisses = new Dev();
+        devUlisses.setNome("Ulisses");
+        devUlisses.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Ulisses:" + devUlisses.getConteudosInscritos());
+
+        devUlisses.progredir();
+        devUlisses.progredir();
+        System.out.println("Conteúdos Inscritos Ulisses:" + devUlisses.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Ulisses:" + devUlisses.getConteudosConcluidos());
+        System.out.println("XP:" + devUlisses.calcularTotalXp());
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
+
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos João:" + devJoao.getConteudosConcluidos());
+        System.out.println("XP:" + devJoao.calcularTotalXp());
+
     }
 }
